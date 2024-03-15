d558413eac7283556f79ff6834dadf9e75e9478f
leehojun <sciboviator@gmail.com>
2024-03-15 15:21:33 +0900

    resourcㄷ커밋시도

--- src/main/java/leehojun/run/Application.java
+++ src/main/java/leehojun/run/Application.java
@@ -1,8 +1,30 @@
-package leehojun;
+package leehojun.run;
+
+import java.util.Scanner;
 
 public class Application {
 
     public static void main(String[] args) {
+
+        EmployeeController employeeController = new EmployeeController();
+        Scanner scr = new Scanner(System.in);
+
+        do {
+
+            System.out.println("====================================================================");
+            System.out.println("1.로그인");
+            System.out.println("2.회원가입");
+            System.out.println("9.프로그램 종료");
+            System.out.print("메뉴를 선택하세요 : ");
+            int no = scr.nextInt();
+            switch (no){
+                case 1:
+                    employeeController.login();
+                    break;
+                case 2: break;
+                case 9: return;
+            }
+        }while(true);
 
 
     }
---
+++ src/main/java/leehojun/run/EmployeeController.java
@@ -0,0 +1,10 @@
+package leehojun.run;
+
+import java.util.Scanner;
+
+public class EmployeeController {
+    public void login() {
+        Scanner scr = new Scanner(System.in);
+        System.out.println("============================================================");
+    }
+}

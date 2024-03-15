a05a1f38952e7fff4f1d4664721b54cd98abbfda
leehojun <sciboviator@gmail.com>
2024-03-15 12:21:52 +0900

    employee 두번째 과제

---
+++ src/main/java/common/Template.java
@@ -0,0 +1,30 @@
+package common;
+
+import org.apache.ibatis.datasource.pooled.PooledDataSource;
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+
+public class Template {
+
+    private static String driver = "com.mysql.cj.jdbc.Driver";
+    private static String url = "jdbc:mysql://localhost/employee";
+    private static String user = "ohgiraffers";
+    private static String pass= "ohgiraffers";
+
+    private static SqlSessionFactory sf;
+
+    public static SqlSession getSqlSession(){
+        if(sf==null){
+            Environment e = new Environment("dev",new JdbcTransactionFactory(),new PooledDataSource(driver,url,user,pass));
+            Configuration config = new Configuration(e);
+            // 자신의 매퍼를 여기에 추가하세요(cofig.addMapper(매퍼명.class)
+            sf = new SqlSessionFactoryBuilder().build(config);
+
+        }
+        return sf.openSession(false);
+    }
+}
---
+++ src/main/java/leehojun/Application.java
@@ -0,0 +1,9 @@
+package leehojun;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+
+    }
+}
